3a4710001401076d1f32c79c056f2a6a3e30c92f
rohansharma-99 <rohan.sharma@trayistats.com>
2026-03-15 22:51:06 +0530

    add-changes1

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -2,7 +2,6 @@ import {BrowserRouter} from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
-import  './style.css'
 import App from './App.jsx';
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
